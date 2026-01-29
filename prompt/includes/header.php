d63fb28b5fac5ba7564d7e11dcfa7b3a80a2fc37
niulok <152132385+niulok@users.noreply.github.com>
2026-01-29 05:15:47 +0300

    Update header.php

--- prompt/includes/header.php
+++ prompt/includes/header.php
@@ -1,1 +1,12 @@
-
+<nav class="dropdown top-navbar">
+  <div class="trademk">
+    <h2>Yimpop</h2>
+  </div>
+  <div class="menu-list">
+    <ul>
+      <?php foreach():?>
+        <li><a href="<?php echo htmlspecialchars()?>"><?php echo htmlspecialchars()?></a></li>
+    <?php endforeach;?>
+    </ul>
+  </div>
+</nav>
